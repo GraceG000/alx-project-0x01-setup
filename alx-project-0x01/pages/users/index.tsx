68be79fb7f2fcf0680bd42a473bac6b67c87f4cc
Grace Ghansah <gracegh@stlghana.com>
2025-11-16 14:22:38 +0000

    updated

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,9 +1,23 @@
 import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
 
-const Users: React.FC = () =>{
-    return(
-        <div></div>
-    )
+interface UsersPageProps {
+  posts: UserProps[]; // this is the users array
+}
+
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
+        return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold mb-4">Users List</h1>
+
+      <div className="grid grid-cols-3 gap-4">
+        {posts.map((user) => (
+          <UserCard key={user.id} {...user} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export async function getStaticProps() {
